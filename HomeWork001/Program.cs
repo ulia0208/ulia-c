2e893633221e59c6be95a367bce88a9a2d1b651f
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-03 12:06:18 +0300

    код первой

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (m>n)
+Console.WriteLine ("Второе число больше");
+
+else
+{
+    Console.WriteLine ("Первое число больше");
+
+}

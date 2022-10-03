b9cea05d61617137734d1e571607f510ca79b87b
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-03 16:10:08 +0300

    задание 1

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -6,8 +6,12 @@ int m = Convert.ToInt32(Console.ReadLine());
 if (m>n)
 Console.WriteLine ("Второе число больше");
 
-else
+else if (m<n)
 {
     Console.WriteLine ("Первое число больше");
 
+}
+else
+{
+     Console.WriteLine ("Числа равны");   
 }

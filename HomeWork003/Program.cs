5ecc46868f13b191064adf3af9414725e3a4e600
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-03 16:27:25 +0300

    Задание 3

---
+++ HomeWork003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите любое число! ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n%2==0)
+{
+    Console.WriteLine("Это четное число");
+}
+
+else
+{
+    Console.WriteLine("Это нечетное число");
+}

12c24c8d00b49981aa716d7417d6d84bf42efe27
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-03 16:06:43 +0300

    задание 2

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Введите первое число! ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Введите второе число! ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число! ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+if (n>m)
+{
+    if (n>k)
+    {
+        Console.WriteLine("Первое - максимальное");
+    }
+    else if (n==k)
+    {
+        Console.WriteLine("Первое и третье равны и максимальны");
+    }
+}
+else if (n==m)
+{
+    if (n>k)
+    {
+        Console.WriteLine("Первое и второе равны и максимальны");
+    }
+    else if (n==k)
+    {
+        Console.WriteLine("Все числа равны");
+    }
+}
+else
+{
+    if (m>k)
+    {
+        Console.WriteLine("Второе - максимальное");
+    }
+    else if (m==k)
+    {
+        Console.WriteLine("Второе и третье равны и максимальны");
+    }
+    else
+    {
+         Console.WriteLine("Третье максимальное");       
+    }
+}
+
